7c657294e7bc8ae870e3d6dbea62d6fe721b1850
IashinME <iashinme@alfastrah.ru>
2023-04-06 00:16:09 +0700

    Clone repository with jGit

---
+++ src/main/java/com/example/jgit/JGitApplication.java
@@ -0,0 +1,45 @@
+package com.example.jgit;
+
+import org.eclipse.jgit.api.Git;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
+
+
+@SpringBootApplication
+public class JGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JGitApplication.class, args);
+
+        final String stringPath = "./git/repository";
+        final String branch = "home-work-02";
+
+        try {
+            File file = Paths.get(stringPath).toFile();
+            deleteDir(file);
+
+            Git.cloneRepository()
+                    .setURI("https://github.com/nnhhard/otus-2022-11-iashinme.git")
+                    .setDirectory(file)
+                    .setBranchesToClone(List.of("refs/heads/" + branch))
+                    .setBranch("refs/heads/" + branch)
+                    .call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteDir(File file) {
+        File[] contents = file.listFiles();
+        if (contents != null) {
+            for (File f : contents) {
+                deleteDir(f);
+            }
+        }
+        file.delete();
+    }
+}
